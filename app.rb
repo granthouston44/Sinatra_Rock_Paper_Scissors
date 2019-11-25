498bc84a70832aa097594ad943c68e907888540c
Grant Houston <grant.houston44@hotmail.com>
2019-11-25 18:32:15 +0000

    added minimal css

--- app.rb
+++ app.rb
@@ -4,14 +4,6 @@ require('sinatra/contrib/all') if development?
 require_relative('models/Game')
 also_reload('./models/*') #if anything in the models folder changes, dont worry about restarting the server - sinatra will do it for us
 
-get '/game/:player1/:player2' do
-player1 = params['player1']
-player2 = params['player2']
-@result = Game.play(player1,player2.computer_player)
-#when calling erb sinatra will look for a views folder with the symbol filename
-erb(:result)
-end
-
 get '/' do
   erb(:home)
 end
